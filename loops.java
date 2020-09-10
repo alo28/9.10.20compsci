6dc18eae0fd13593b8b078c0a3966657b2ab6eaf
alo28 <71102841+alo28@users.noreply.github.com>
2020-09-10 15:39:32 -0600

    Create loops.java

---
+++ loops.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class loops{
+
+  public static void main (String [] args) {
+  
+      Scanner inpu = new Scanner (System.in);
+      String a = input.nextline();
+      System.out.println("wuw");
+      
+      System.out.println(a);
